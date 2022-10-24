f08c1e3fb6c20439eb0ee8ff74c536a92e734aeb
BrenieEmnace <emnacebrenie@gmail.com>
2022-10-24 11:51:27 +0800

    first commit

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,47 @@
+
+@extends('base')
+
+@section('content')
+
+<style>
+  img {
+  width: 100%;
+  height: auto;
+}
+
+.jumbotron {
+    background: url("Beach.jpg") no-repeat center;
+}
+.btn-primary {
+    background-color: #c60063;
+    border-color: #c60063;
+}
+
+.btn-secondary {
+    background-color: transparent;
+    border-color: #c60063;
+    color: #c60063;
+}
+
+#services {
+    background: url(".jpg");
+}
+
+body{
+   background-image:url('book.jpg');
+   background-size: cover;
+}
+
+
+</style>
+
+<br>
+<body>
+   <section id="services" class="container">
+      <h2 class="display-4 text-center mt-5 mb-3"><strong>Welcome to CheapTalk!</strong></h2>
+      <h5 style="font-weight: 400"> Express your emotion here.
+    </section>
+    
+</body>
+
+
